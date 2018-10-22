2f9029020c9197667cd612ca204a4202d2090fcb
汪俊 <WangJun19850215@163.com>
2018-10-22 23:04:33 +0800

    11

---
+++ WangJun.APITest/WeChatAPI.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WangJun.APITest
+{
+    /// <summary>
+    /// 微信接口
+    /// </summary>
+    public class WeChatAPI
+    {
+
+    }
+}
---
+++ WebAPI/WeChat.ashx.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+
+namespace WebAPI
+{
+    /// <summary>
+    /// WeChat 的摘要说明
+    /// </summary>
+    public class WeChat : IHttpHandler
+    {
+
+        public void ProcessRequest(HttpContext context)
+        {
+            var signature = context.Request["signature"];
+            var timestamp = context.Request["timestamp"];
+            var nonce = context.Request["nonce"];
+            var echostr = context.Request["echostr"];
+            File.WriteAllText(string.Format("{0}/yyyyMMddhhmmss.txt",context.Server.MapPath("~"), DateTime.Now), context.Request.RawUrl);
+            context.Response.ContentType = "text/plain";
+            context.Response.Write(echostr);
+        }
+
+        public bool IsReusable
+        {
+            get
+            {
+                return false;
+            }
+        }
+    }
+}

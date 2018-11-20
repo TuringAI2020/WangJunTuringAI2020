1bb8ba3227f6924959a6d69e00acf3af17be7ed7
汪俊 <turingai@outlook.com>
2018-11-21 07:12:03 +0800

    添加表单创建器

---
+++ WangJun.PCWeb/YunForm/YunForm.js
@@ -0,0 +1,16 @@
+﻿var YunForm = {};
+YunForm.VUE = null;
+YunForm.BuildForm = function (option) {
+ 
+    YunForm.VUE = app;
+}
+
+YunForm.BuildForm();
+
+var app = new Vue({
+    el: '#form',
+    data: {
+        showform: false,
+        message: 'Hello Vue!'
+    }
+});

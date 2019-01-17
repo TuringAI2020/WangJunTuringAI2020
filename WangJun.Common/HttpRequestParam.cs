498587bd1fc4fc772684f017c3d89b3313dd9c31
俊 汪 <turingai@outlook.com>
2019-01-18 07:22:53 +0800

    .

---
+++ WangJun.Common/HttpRequestParam.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WangJun.Yun
+{
+    public class HttpRequestParam
+    {
+        public Dictionary<string, object> GetQueryParam { get; set; }
+
+        public Dictionary<string, object> PostFormParam { get; set; }
+
+        public Dictionary<string, object> PostJsonStreamParam { get; set; }
+
+        public static HttpRequestParam Parse(Stream stream,Encoding encoding=null) {
+            var inst = new HttpRequestParam();
+            if (null != stream)
+            {
+                if (null == encoding)
+                {
+                    encoding = Encoding.UTF8;
+                }
+
+                var bytes = new byte[stream.Length];
+                stream.Read(bytes, 0, (int)stream.Length);
+                var str = encoding.GetString(bytes).Trim();
+                if ((str.StartsWith("{") && str.EndsWith("}")) || (str.StartsWith("[") && str.EndsWith("]")))
+                {
+                    var streamData = JSON.ToObject<Dictionary<string, object>>(str);
+                    inst.PostJsonStreamParam = streamData;
+                }
+                else if (str.Contains("="))
+                {
+                    var array = str.Split(new char[] { '&' }, StringSplitOptions.RemoveEmptyEntries);
+                    foreach (var item in collection)
+                    {
+
+                    }
+                }
+            }
+
+            return inst;
+        }
+    }
+     
+}
--- WangJun.YunFile/YunFile.cs
+++ WangJun.YunFile/YunFile.cs
@@ -11,16 +11,22 @@ namespace WangJun.Yun
 {
     public class YunFile
     {
-        public string SaveFromHttp() {
+        /// <summary>
+        /// 存储文件
+        /// </summary>
+        /// <returns></returns>
+        public string SaveFromHttp()
+        {
             var context = HttpContext.Current;
             var filePath = string.Empty;
-            if (null != context&& null != context.Request.Files ) {
+            if (null != context && null != context.Request.Files)
+            {
                 for (int k = 0; k < context.Request.Files.Count; k++)
                 {
                     HttpPostedFile file = context.Request.Files[k];
 
 
-                    var path = context.Server.MapPath("~")+"/"+"FILES/";
+                    var path = context.Server.MapPath("~") + "/" + "FILES/";
                     if (!Directory.Exists(path))
                     {
                         Directory.CreateDirectory(path);
@@ -28,14 +34,35 @@ namespace WangJun.Yun
                     var bytes = new byte[(int)file.InputStream.Length];
                     file.InputStream.Read(bytes, 0, bytes.Length);
                     var fileName = MD5.ToMD5(bytes);
-                    if (file.FileName.Contains(".")) {
+                    if (file.FileName.Contains("."))
+                    {
                         fileName = fileName + "." + file.FileName.Split(new char[] { '.' }, StringSplitOptions.RemoveEmptyEntries)[1];
                     }
-                    filePath = context.Request.Url.Authority+ "/FILES/" + fileName;
-                    file.SaveAs(path+fileName);
+                    filePath = context.Request.Url.Authority + "/FILES/" + fileName;
+                    file.SaveAs(path + fileName);
+
+                    #region 写文件信息
+                    var fileInfo = new StringBuilder();
+                    fileInfo.AppendLine(string.Format("请求原始地址\t{0}",context.Request.RawUrl));
+                    fileInfo.AppendLine(string.Format("文件名\t{0}", file.FileName));
+                    fileInfo.AppendLine(string.Format("后缀名\t{0}", file.FileName.Contains(".")? file.FileName.Split(new char[] { '.' }, StringSplitOptions.RemoveEmptyEntries)[1]:string.Empty));
+                    fileInfo.AppendLine(string.Format("存储路径\t{0}", filePath));
+                    fileInfo.AppendLine(string.Format("存储文件名\t{0}", fileName));
+                    fileInfo.AppendLine(string.Format("分片文件MD5\t{0}", fileName));
+                    fileInfo.AppendLine(string.Format("原始文件大小\t{0}", file.InputStream.Length));
+                    fileInfo.AppendLine(string.Format("组文件MD5\t{0}", fileName));
+                    File.WriteAllText(path + fileName + ".txt", fileInfo.ToString());
+                    #endregion
                 }
             }
             return filePath;
         }
+
+        /// <summary>
+        /// 删除文件
+        /// </summary>
+        /// <returns></returns>
+        public string DeleteFromHttp() {
+         }
     }
 }

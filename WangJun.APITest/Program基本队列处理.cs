5ca371a7673e2a9f534c360731653abaabdc4728
俊 汪 <turingai@outlook.com>
2019-03-30 09:39:20 +0800

    添加自宿主应用

--- WangJun.APITest/Program基本队列处理.cs
+++ WangJun.APITest/Program基本队列处理.cs
@@ -1,8 +1,11 @@
-﻿using System;
+﻿using Microsoft.Owin.Hosting;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
+using WangJun.SelfHostAPI;
 using WangJun.Yun;
 
 namespace WangJun.APITest
@@ -11,6 +14,26 @@ namespace WangJun.APITest
     {
         static void Main(string[] args)
         {
+            string baseAddress = "http://localhost:9000/";
+
+            // Start OWIN host 
+            using (WebApp.Start<Startup>(url: baseAddress))
+            {
+                // Create HttpCient and make a request to api/values 
+                HttpClient client = new HttpClient();
+
+                var response = client.GetAsync(baseAddress + "api/values").Result;
+
+                Console.WriteLine(response);
+                Console.WriteLine(response.Content.ReadAsStringAsync().Result);
+                Console.ReadLine();
+
+            }
+            return;
+
+            七彩福星.计算();
+
+            return;
 
             var allChar = HANZI.GetAll();
             ///创建1000个商品
---
+++ WangJun.APITest/七彩福星.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WangJun.Yun;
+
+namespace WangJun.APITest
+{
+    public static class 七彩福星
+    {
+        public static void 计算() {
+            for (int k = 0; k < 1000*10000; k++)
+            {
+                var guidArr = Guid.NewGuid().ToByteArray();
+                var val = Math.Abs(BitConverter.ToInt32(guidArr, (k + 4) % 12));
+                if (400 * 10000 <= val && val <= 9999999)
+                {
+                    Console.WriteLine(val);
+                }
+ 
+            }
+         }
+    }
+}

80ef7065974647610d15c4c474fface390b36398
俊 汪 <turingai@outlook.com>
2019-04-06 14:31:19 +0800

    .

--- WangJun.PCWeb/YunAppTemplate/JS/YunService/YunCategoryService.js
+++ WangJun.PCWeb/YunAppTemplate/JS/YunService/YunCategoryService.js
@@ -13,52 +13,43 @@ option.methods = {};
 option.created = function () { }
 
 
-option.methods.LoadCategoty = function () {
+option.methods.LoadCategotyList = function () {
     let vThis = this;
     vThis.page.alert.text = "OK124";
-    let param = {
-        FormType: parseInt("0x0301", 16),
-        Title: vThis.data.title,
-        ValueS01: UM.getEditor('myEditor').getContent(),
-        ValueS02: UM.getEditor('myEditor').getContentTxt()
-    };
-
-    param.ValueS03 = (150 <= param.ValueS02.length) ? param.ValueS02.substring(0, 150) : param.ValueS02;
-
-
-
+ 
+     
     let ajaxOption = {
         url: "http://localhost:5168/API.ashx",
         method: "POST",
         dataType: "json",
         data: JSON.stringify({
             "TargetClass": "YunRelation",
-            "Method": "Save",
-            "Param": param,
-            "InputParamArray": []
+            "Method": "LoadList",
+            "Param": {},
+            "InputParamArray": ['F06E86A8-DB7F-4E39-BA1E-C2F620F8D20C']
         })
     };
     $.ajax(ajaxOption)
         .done(function (res1, res2) {
             vThis.page.alert.text = "OK123";
+            let zNodes = res1.DATA;
+            LoadZtree("#tree1", zNodes);
+            LoadZtree("#tree2", zNodes);
+            LoadZtree("#tree3", zNodes);
+
         }).fail(function (res1, res2) {
 
         }).always(function (res1, res2) { });
 }
 
-option.methods.SaveDocument = function () {
+option.methods.SaveCategory = function () {
     let vThis = this;
     vThis.page.alert.text = "OK124";
-    let param = {
-        FormType: parseInt("0x0301", 16),
-        Title: vThis.data.title,
-        ValueS01: UM.getEditor('myEditor').getContent(),
-        ValueS02: UM.getEditor('myEditor').getContentTxt()
+    let param = { 
+        Name: "汪俊头条2",
+        ParentID:"593dfbfd-3b26-470c-b9e5-1017c4074419"
     };
-
-    param.ValueS03 = (150 <= param.ValueS02.length) ? param.ValueS02.substring(0, 150) : param.ValueS02;
-
-
+     
 
     let ajaxOption = {
         url: "http://localhost:5168/API.ashx",
@@ -82,4 +73,31 @@ option.methods.SaveDocument = function () {
 
 
 let vue = new Vue(option);
+
+function LoadZtree(el, zNodes)
+{ 
+    var zSetting = {
+
+        data: {
+            simpleData: {
+                enable: true,
+                idKey: "ID",
+                pIdKey: "ParentID",
+                rootPId: 0
+            },
+            key: {
+                name: "Name"
+            }
+        },
+        callback: {
+            onClick: function (dat1, dat2, dat3) {
+
+            }
+        }
+    };
+     
+    zNodes.forEach(p => { p.open = true; });
+
+    $.fn.zTree.init($(el), zSetting, zNodes);
+}
  

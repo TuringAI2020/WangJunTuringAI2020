78d296b876181589bbf773fdae07041ffc04d98f
俊 汪 <turingai@outlook.com>
2019-04-24 01:03:04 +0800

    添加新页面

--- WangJun.Common/ENUM.cs
+++ WangJun.Common/ENUM.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace WangJun.Yun
+﻿namespace WangJun.Yun
 {
     public static class ENUM
     {
@@ -16,7 +10,12 @@ namespace WangJun.Yun
         {
             云收藏 = 0x0101,
             云商品 = 0x0201,
-            云头条=0x0301
+            云头条 = 0x0301,
+            云应用 = 0x0401,
+            云菜单 = 0x0501,
+            云账号 = 0x0601,
+            云订单 = 0x0701,
+            云权限 = 0x0801,
         }
 
         public enum TaskStatus
@@ -52,7 +51,7 @@ namespace WangJun.Yun
         public enum CommodityStatus
         {
             上架 = 0x0010,
-            待售=0x0020,
+            待售 = 0x0020,
             下架 = -0x0010
         }
 
@@ -69,13 +68,13 @@ namespace WangJun.Yun
         /// </summary>
         public enum 订单及消费项状态
         {
-            异常=-0x10000,
-            未支付=0x10000,
-            未使用=0x10001,
-            部分使用=0x10002,
-            无可用项目=0x10003,///全部被用,部分被用,部分退款
-            全部已退款=0x10004,
-            已取消=0x10005,
+            异常 = -0x10000,
+            未支付 = 0x10000,
+            未使用 = 0x10001,
+            部分使用 = 0x10002,
+            无可用项目 = 0x10003,///全部被用,部分被用,部分退款
+            全部已退款 = 0x10004,
+            已取消 = 0x10005,
             退款处理中 = 0x10006,
             已退款 = 0x10007,
         }
---
+++ WangJun.PCWeb/YunAppTemplate/JS/YunService/YunAppService.js
@@ -0,0 +1,28 @@
+﻿let vTop = new Vue({
+    el: "#top",
+    data: {
+        app: {
+            Name:"应用名称"
+        },
+        Menu1: [{ ID: "", Name: "新增" }],
+        Menu2: [{ ID: "", Name: "新增" },{ ID: "", Name: "注销" }],
+    }
+});
+
+let vLeft = new Vue({
+    el: "#left",
+    data: { 
+        Menu1: [{ ID: "", Name: "新增" }], 
+    }
+});
+
+
+let vNav = new Vue({
+    el: "#nav",
+    data: {
+        func: {
+            Name: "功能名称"
+        },
+        Menu1: [{ ID: "", Name: "新增" }],
+    }
+});
